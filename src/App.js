66c2a5f0df68d46dac57398c3d407a5c0d4cd3ae
zidoxx <estebancastriver@gmail.com>
2021-05-29 10:41:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+    const [number, setNum] = useState(0);
+
+    const showNumbers = (num) => {
+        let arr = [];
+        for (let i = 1; i <= num; i++) {
+            arr.push(i);
+        }
+        return arr;
+    };
+
+    const showData = () => {
+        setData(showNumbers(number));
+    };
+
+    useEffect(() => {
+        showData();
+    }, []);
+
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <input
+                    type='text'
+                    placeholder='Ingrese un numero'
+                    onChange={(e) => setNum(e.target.value)}
+                />
+
+                <button onClick={showData}>Click</button>
+
+                {data && data.map((e) => <p>Numero: {e}</p>)}
+
+                <a
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/functions/oneToTen.js
@@ -0,0 +1,9 @@
+const showNumbers = (data) => {
+    let arr = [];
+    for (let i = 1; i <= data; i++) {
+        arr.push(i);
+    }
+    return arr;
+};
+
+export default showNumbers;
